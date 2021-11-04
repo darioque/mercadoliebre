5719a57c7e691fb1f85fa2edacc0a41356a5a9db
Darioque <darioskidelsky@gmail.com>
2021-11-04 19:57:01 -0300

    changing ports to deploy

--- app.js
+++ app.js
@@ -5,14 +5,11 @@ const app = express();
 
 const publicPath = path.join(__dirname, "./public");
 
-app.use(express.static(publicPath)); // public es nuestro recurso de archivos estaticos
+app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log("El servidor se está ejecutando en el puerto 3000");
-});
+app.listen(process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-  // responde get to root with index.html
   res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 app.post("/", (req, res) => {
@@ -20,11 +17,9 @@ app.post("/", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  // responde get to root with index.html
   res.sendFile(path.join(__dirname, "/views/register.html"));
 });
 
 app.get("/login", (req, res) => {
-  // responde get to root with index.html
   res.sendFile(path.join(__dirname, "/views/login.html"));
 });
